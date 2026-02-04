5c7cfedf850fcb7a38b6648db2d4155c1e3f4308
Louis Billaut <ragedfear4875@gmail.com>
2026-02-05 04:20:12 +0800

    added dynamic lighting

--- src/main/java/net/louis/overhaulmod/mixin/ServerPlayerTickMixin.java
+++ src/main/java/net/louis/overhaulmod/mixin/ServerPlayerTickMixin.java
@@ -1,20 +1,66 @@
 package net.louis.overhaulmod.mixin;
 
 import net.louis.overhaulmod.item.ModItems;
+import net.louis.overhaulmod.utils.GlowLightManager;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.LightBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ServerPlayerEntity.class)
 public class ServerPlayerTickMixin {
+    @Unique @Nullable private BlockPos lastLightPos = null;
+
+    ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
+
     @Inject(method = "tick", at = @At("TAIL"))
     private void LOM$allowRecallClock(CallbackInfo ci) {
-        ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
-        if (player.getHealth() < player.getMaxHealth()) {
-            player.getItemCooldownManager().set(new ItemStack(ModItems.RECALL_CLOCK.asItem()), 100);
+        if (this.player.getHealth() < this.player.getMaxHealth()) {
+            this.player.getItemCooldownManager().set(new ItemStack(ModItems.RECALL_CLOCK.asItem()), 100);
         }
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void LOM$dynamicLighting(CallbackInfo ci) {
+        ServerWorld serverWorld = this.player.getServerWorld();
+        Integer lightLevel = hasLightItem(this.player);
+        if (lightLevel == null) lightLevel = 0;
+
+        BlockPos playerPos = this.player.getBlockPos();
+        BlockPos upPos = playerPos.up();
+
+        if (this.lastLightPos != null) {
+            serverWorld.setBlockState(this.lastLightPos, Blocks.AIR.getDefaultState());
+            this.lastLightPos = null;
+        }
+
+        BlockPos lightPos = null;
+        if (serverWorld.getBlockState(playerPos).isAir()) {
+            lightPos = playerPos;
+        } else if (serverWorld.getBlockState(upPos).isAir()) {
+            lightPos = upPos;
+        }
+
+        if (lightPos != null && serverWorld.getBlockState(lightPos) != Blocks.LIGHT.getDefaultState().with(LightBlock.LEVEL_15, lightLevel)) {
+            if (lightLevel > 0) {
+                serverWorld.setBlockState(lightPos, Blocks.LIGHT.getDefaultState().with(LightBlock.LEVEL_15, lightLevel));
+                this.lastLightPos = lightPos;
+            }
+            else
+                serverWorld.setBlockState(lightPos, Blocks.AIR.getDefaultState());}
+    }
+
+    private Integer hasLightItem(ServerPlayerEntity player) {
+        Integer level = GlowLightManager.ITEM_LIGHT_LEVELS.get(player.getMainHandStack().getItem());
+        if (level != null) return level;
+        return GlowLightManager.ITEM_LIGHT_LEVELS.get(player.getOffHandStack().getItem());
     }
 }
