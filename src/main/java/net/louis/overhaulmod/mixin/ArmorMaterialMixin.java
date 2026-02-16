3acadb4b95e133161f30cc5f298bc6311d955de2
Louis Billaut <ragedfear4875@gmail.com>
2026-02-16 15:04:28 +0800

    updated: added kb res to chainmail

---
+++ src/main/java/net/louis/overhaulmod/mixin/ArmorMaterialMixin.java
@@ -0,0 +1,41 @@
+package net.louis.overhaulmod.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.louis.overhaulmod.LouisOverhaulMod;
+import net.minecraft.component.type.AttributeModifierSlot;
+import net.minecraft.component.type.AttributeModifiersComponent;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.equipment.ArmorMaterial;
+import net.minecraft.item.equipment.ArmorMaterials;
+import net.minecraft.item.equipment.EquipmentType;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ArmorMaterial.class)
+public class ArmorMaterialMixin {
+    ArmorMaterial self = (ArmorMaterial) (Object) this;
+
+    @ModifyReturnValue(method = "createAttributeModifiers", at = @At("RETURN"))
+    private AttributeModifiersComponent LOM$addChainmailKnockbackResistance(AttributeModifiersComponent original, EquipmentType equipmentType) {
+        if (self == ArmorMaterials.CHAIN) {
+            EquipmentSlot slot = equipmentType.getEquipmentSlot();
+            AttributeModifiersComponent.Builder builder = AttributeModifiersComponent.builder();
+
+            for (AttributeModifiersComponent.Entry entry : original.modifiers()) builder.add(entry.attribute(), entry.modifier(), entry.slot());
+
+            builder.add(
+                    EntityAttributes.KNOCKBACK_RESISTANCE,
+                    new EntityAttributeModifier(
+                            Identifier.of(LouisOverhaulMod.MOD_ID, "chainmail_kb_resistance"),
+                            0.25, EntityAttributeModifier.Operation.ADD_VALUE
+                    ),
+                    AttributeModifierSlot.forEquipmentSlot(slot)
+            );
+            return builder.build();
+        }
+        return original;
+    }
+}
