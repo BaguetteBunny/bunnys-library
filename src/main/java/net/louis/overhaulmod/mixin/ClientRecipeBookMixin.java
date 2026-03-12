10bc72f1fa6fa5aea8c449401b98f1859432e989
Louis Billaut <ragedfear4875@gmail.com>
2026-03-12 15:23:12 +0800

    remove client recipe book mixin

--- src/main/java/net/louis/overhaulmod/mixin/ClientRecipeBookMixin.java
+++
@@ -1,9 +0,0 @@
-package net.louis.overhaulmod.mixin;
-
-import net.minecraft.client.recipebook.ClientRecipeBook;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ClientRecipeBook.class)
-public class ClientRecipeBookMixin {
-
-}
